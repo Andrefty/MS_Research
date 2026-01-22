96e723cf954fbdc7ba0aa6b7c73ae3c964f257fc
Andrefty <39839328+Andrefty@users.noreply.github.com>
2026-01-22 08:04:12 +0200

    Another batchsize adjustment, min_p param fix doesn't work, anyway default is 0.0 in vllm doc as per link in previous commit

--- training_grpo/job_train_grpo.sh
+++ training_grpo/job_train_grpo.sh
@@ -97,7 +97,7 @@ apptainer exec --nv \
             actor_rollout_ref.actor.optim.lr=1e-6 \
             actor_rollout_ref.model.use_remove_padding=True \
             actor_rollout_ref.model.enable_gradient_checkpointing=True \
-            actor_rollout_ref.actor.ppo_mini_batch_size=16 \
+            actor_rollout_ref.actor.ppo_mini_batch_size=12 \
             actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=4 \
             actor_rollout_ref.actor.use_kl_loss=False \
             actor_rollout_ref.actor.kl_loss_coef=0.0 \
@@ -112,7 +112,6 @@ apptainer exec --nv \
             actor_rollout_ref.rollout.temperature=0.6 \
             actor_rollout_ref.rollout.top_p=0.95 \
             actor_rollout_ref.rollout.top_k=20 \
-            +actor_rollout_ref.rollout.engine_kwargs.vllm.min_p=0.0 \
             actor_rollout_ref.rollout.n=4 \
             actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=8 \
             actor_rollout_ref.rollout.disable_log_stats=False \
